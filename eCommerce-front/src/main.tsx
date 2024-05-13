6d4ff0c74824d969966b684421a6f69852126018
Amal-Elafifi <amalelafifi91@gmail.com>
2024-05-13 11:27:36 +0200

    adding routes to AppRouter

--- eCommerce-front/src/main.tsx
+++ eCommerce-front/src/main.tsx
@@ -1,42 +1,9 @@
 import ReactDOM from "react-dom/client";
+import AppRouter from "@routes/AppRouter";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { MainLayout } from '@layouts/index';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Home from '@pages/Home';
-import Categories from '@pages/Categories';
-import Products from '@pages/Products';
-import AboutUs from '@pages/AboutUs';
 
 
 
-const router =createBrowserRouter([
-    {
-        path: "/",
-        element: <MainLayout/>,
-        children: [
-            {
-                index: true,
-                element: <Home/>,
-            },
-            {
-                path: "categories",
-                element: <Categories/>,
-            },
-            {
-                path: "products/:prefix",
-                element: <Products/>,
-            },
-            {
-                path: "about-us",
-                element: <AboutUs/>,
-            }
-        ]
-    }
-])
-
-
-
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<RouterProvider router={router}/>);
+ReactDOM.createRoot(document.getElementById("root")!).render(<AppRouter/>);
 
