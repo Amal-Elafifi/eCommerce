0fb30740f3135a84e2c015d332cfffbf31af7bbe
Amal-Elafifi <amalelafifi91@gmail.com>
2024-05-15 09:27:55 +0200

    add categories and products

--- eCommerce-front/src/pages/Error.tsx
+++ eCommerce-front/src/pages/Error.tsx
@@ -1,12 +1,26 @@
-import "@styles/global.css";
+import { Link, isRouteErrorResponse, useRouteError } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import "@styles/global.css";
 
 const Error = () => {
+  const error = useRouteError();
+  let errorStatus: number;
+  let errorStatusText: string;
+
+  if(isRouteErrorResponse(error)){
+    errorStatus= error.status;
+    errorStatusText= error.statusText
+  }else{
+    errorStatus= 404;
+    errorStatusText= "Page not found";
+  }
+
+
   return (
     <Container className="notFound">
-      <h1>404</h1>
-     <Link to="/" replace={true}>Looks like you have reached to non-existing page, <br/>
+      <h1>{errorStatus}</h1>
+      <p>{errorStatusText}</p>
+     <Link to="/" replace={true}><br/>
         how about going back to safety?
      </Link>
     </Container>
---
+++ eCommerce-front/src/pages/index.ts
@@ -0,0 +1,5 @@
+import Product from "@components/ecommerce/headerbasket/product/Product";
+import Category from "@components/ecommerce/category/Category";
+
+
+export { Product, Category};
