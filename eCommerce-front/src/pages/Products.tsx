15c5097ce1f31d5b38eeb2f964002a665a92b06c
Amal-Elafifi <amalelafifi91@gmail.com>
2024-05-26 10:18:23 +0200

    after adding generics

--- eCommerce-front/src/pages/Products.tsx
+++ eCommerce-front/src/pages/Products.tsx
@@ -1,40 +1,32 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Container} from "react-bootstrap";
 import { useAppDispatch, useAppSelector } from "@store/hooks";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import {actGetProductsbyCatPrefix, productsCleanup } from "@store/products/productsSlice";
+import {actGetProductsbyCatPrefix, productsCleanup} from ".";
 import { Product } from ".";
 import { Loading } from "@components/feedback";
-
+import { GridList} from ".";
 
 const Products = () => {
-    const dispatch = useAppDispatch();
-    const params = useParams();
-    const {loading, records, error} = useAppSelector(state => state.product);
-  
-    useEffect(() => {
-      dispatch(actGetProductsbyCatPrefix(params.prefix as string))
-      return () => {
-        dispatch(productsCleanup())
-      } 
-    }, [dispatch, params]);
+  const dispatch = useAppDispatch();
+  const params = useParams();
+  const { loading, records, error } = useAppSelector((state) => state.product);
 
-    const productsList = records.length > 0 ? records.map(record => (
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-            <Product {...record} />
-        </Col>
-    )) :"product not found ";
+  useEffect(() => {
+    dispatch(actGetProductsbyCatPrefix(params.prefix as string));
+    return () => {
+      dispatch(productsCleanup());
+    };
+  }, [dispatch, params]);
 
-    
-    return (
-        <Container>
-          <Loading status={loading} error={error}>
-            <Row>
-                {productsList}
-            </Row>
-          </Loading>
-        </Container>
+
+  return (
+    <Container>
+      <Loading status={loading} error={error}>
+        <GridList records={records} renderItem={(record) => <Product {...record} />}/>
+      </Loading>
+    </Container>
   );
-}
+};
 
 export default Products;
