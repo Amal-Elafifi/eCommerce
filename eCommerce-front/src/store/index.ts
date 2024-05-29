24bf5f01254fa059bd2472e5d46568ba362857c0
Amal-Elafifi <amalelafifi91@gmail.com>
2024-05-29 09:57:58 +0200

    using redux persist for storing data

--- eCommerce-front/src/store/index.ts
+++ eCommerce-front/src/store/index.ts
@@ -1,16 +1,48 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
+import { persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+ } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import categories from "./categories/categoriesSlice";
 import product from "./products/productsSlice";
-import Cart from "./cart/CartSlice"
+import Cart from "./cart/CartSlice";
 
-const store = configureStore({
-  reducer: {categories, product, Cart}
+
+
+const cartPersistConfig = {
+  key: "Cart",
+  storage,
+  whiteList: ["items"]
+}
+
+const rootReducer = combineReducers({
+  categories,
+  product,
+  Cart: persistReducer(cartPersistConfig, Cart)
 });
 
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [ FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  })
+})
+
+const persistor = persistStore(store);
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
-
-export default store;
+export { store, persistor };
