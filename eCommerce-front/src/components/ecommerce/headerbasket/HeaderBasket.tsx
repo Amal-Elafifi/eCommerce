7919fe6a067dc9827b2455a1c4ce03039bc31f19
Amal-Elafifi <amalelafifi91@gmail.com>
2024-05-29 11:23:01 +0200

    addig spinner to add to cart btn

--- eCommerce-front/src/components/ecommerce/headerbasket/HeaderBasket.tsx
+++ eCommerce-front/src/components/ecommerce/headerbasket/HeaderBasket.tsx
@@ -1,17 +1,31 @@
-import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
 import { useAppSelector } from "@store/hooks";
-import { Logo } from ".";
-import { cartItemsQuantitySelector } from "@store/cart/selectors/cartItemsSelector";
+import { Logo, cartItemsQuantitySelector } from ".";
+import styles from "./styles.module.css";
 
-const { basketContainer, basketQuantity } = styles;
+const { basketContainer, basketQuantity, pumpCartQuantity } = styles;
 
 const HeaderBasket = () => {
+  const[isAnimated, setIsAnimated] = useState(false);
   const totalQuantitiy = useAppSelector(cartItemsQuantitySelector);
+      <div className={basketQuantity}>{totalQuantitiy}</div>
+  const quantityStyle =`${basketQuantity} ${isAnimated? pumpCartQuantity: "" }`
+
+  useEffect(() => {
+    if(!totalQuantitiy){
+      return;
+    }
+    setIsAnimated(true);
+    const debounced = setTimeout(()=>{
+        setIsAnimated(false)
+    }, 300)
+    return () => clearTimeout(debounced)
+  },[totalQuantitiy])
 
   return (
     <div className={basketContainer}>
       <Logo />
-      <div className={basketQuantity}>{totalQuantitiy}</div>
+      <div className={quantityStyle}>{totalQuantitiy}</div>
     </div>
   );
 };
