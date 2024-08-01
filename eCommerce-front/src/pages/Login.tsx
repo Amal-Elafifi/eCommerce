cc88414ecffa6aa957e1b17cbac4a3f1cb82c0c2
Amal-Elafifi <amalelafifi91@gmail.com>
2024-08-01 11:10:35 +0200

    login action

--- eCommerce-front/src/pages/Login.tsx
+++ eCommerce-front/src/pages/Login.tsx
@@ -1,22 +1,48 @@
+import { useEffect } from 'react';
+import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '@store/hooks';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import toast, {Toaster} from "react-hot-toast";
+import {Form, Button, Row, Col} from 'react-bootstrap';
 import { Heading } from '@components/common';
 import { Input } from '@components/form/input';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { signinScheme, signinType } from '@validations/SigninScheme';
-import {Form, Button, Row, Col} from 'react-bootstrap';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { actAuthLogin } from '@store/auth/authSlice';
 
 const Login = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate= useNavigate();
+  const dispatch = useAppDispatch();
   const {register, handleSubmit, formState: {errors}} = useForm<signinType>({
       resolver: zodResolver(signinScheme),
       mode: "onBlur"
   });
-  const formSubmit: SubmitHandler<signinType> = (data) => {console.log(data)}
+
+  useEffect(()=>{
+    const message = searchParams.get("message")
+    if( message === "successfully_created"){
+      toast.success("Your account successfully created. please login", {
+        position: "top-center"
+      })
+    } 
+  },[searchParams])
+
+  const formSubmit: SubmitHandler<signinType> = (data) => {
+    console.log(data);
+    dispatch(actAuthLogin(data)).unwrap().then(()=>{
+      navigate("/")
+    })
+
+  }
+
 
   return (
     <>
     <Heading title="User Login"/>
       <Row>
         <Col md={{span: 6, offset: 3}}>
+        <Toaster/>
           <Form onSubmit={handleSubmit(formSubmit)}>
             <Input 
               label="Email address"
